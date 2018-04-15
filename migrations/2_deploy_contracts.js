5579b61d7cf8480cffd54230483cff8c0c4000b6
Austin Hwang <austin.y.hwang@gmail.com>
2018-04-15 15:18:18 -0400

    Fix

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,6 +1,7 @@
 var SimpleStorage = artifacts.require("./SimpleStorage.sol");
 var TutorialToken = artifacts.require("./TutorialToken.sol");
 var ComplexStorage = artifacts.require("./ComplexStorage.sol");
+var Auction = artifacts.require("./Auction.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(SimpleStorage);
