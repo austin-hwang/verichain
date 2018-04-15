988cc02a5d861b9da192b5a6d0ea9ca70b4594f8
Nathan Lee <NLee@Nathans-MacBook-Pro.local>
2018-04-15 15:34:08 -0400

    working boilerplate

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,11 +1,11 @@
 var SimpleStorage = artifacts.require("./SimpleStorage.sol");
 var TutorialToken = artifacts.require("./TutorialToken.sol");
 var ComplexStorage = artifacts.require("./ComplexStorage.sol");
-var Auction = artifacts.require("./Auction.sol");
+var SimpleAuction = artifacts.require("./SimpleAuction.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(SimpleStorage);
   deployer.deploy(TutorialToken);
   deployer.deploy(ComplexStorage);
-  deployer.deploy(Auction);
+  deployer.deploy(SimpleAuction, 1000, 0xfafee0289b7b027aedfb25605013011bf0e8e102);
 };
