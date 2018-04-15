72b2784efa870936cc5d2398e07f60986a28a6b1
Austin Hwang <austin.y.hwang@gmail.com>
2018-04-15 15:08:52 -0400

    Update boilerplate

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,7 +1,6 @@
 var SimpleStorage = artifacts.require("./SimpleStorage.sol");
 var TutorialToken = artifacts.require("./TutorialToken.sol");
 var ComplexStorage = artifacts.require("./ComplexStorage.sol");
-var Auction = artifacts.require("./Auction.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(SimpleStorage);
